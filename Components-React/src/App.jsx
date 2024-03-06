ac37a3036f412070ace1e464284e90eada32440f
KaterinaB10 <katerina.b10.kodehode@gmail.com>
2024-03-06 09:01:02 +0100

    first commit

---
+++ Components-React/src/App.jsx
@@ -0,0 +1,20 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div className="app">
+      <header className="header">
+        <h1>Components</h1>
+      </header>
+
+      <main></main>
+
+      <footer className="footer">
+        <p>Made by Katerina</p>
+        <p>Using React</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
